9b68474302b2a9072db9135062195a926fb54fb3
Wahyu Hidayat <wahhid@gmail.com>
2020-07-18 10:59:45 +0700

    Update __manifest__.py

--- weha_visitor_mgmt/__manifest__.py
+++ weha_visitor_mgmt/__manifest__.py
@@ -1,27 +1,5 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2019-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Anusha P P (odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
-
 {
-    'name': "Front Office Management",
+    'name': "Visitor Management",
     'version': '13.0.1.0.1',
     'summary': """Manage Building Visitor""",
     'description': """Helps You To Manage Front Office Operations , Odoo 13""",
